4b2e93bdd6a37cdf4cba0d852a8d4864ec02f4b6
Malachi Francis <malachifrancis.se@gmail.com>
2018-10-29 17:16:29 -0400

    decorator testing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from objects import Node
+from objects import Node, Tree
 from collections import deque
+from decorators import profile_me, delay_me
+import gc
+
+gc.disable()
 
 node1 = Node(1)
 node4 = Node(4)
@@ -15,39 +19,58 @@ node5 = Node(5)
 node5.set_left(node3)
 node5.set_right(node8)
 
-# node5 is root of a complete balanced binary search tree
-# bfs() should return:
-# 5
-# 38
-# 1469
 
-def bfs(root):
+@profile_me
+def bft_exec(root):
+    bft(root)
+
+
+def bft(root):
+    """Breadth first traversal of a binary tree"""
     clevel = 1
     root.set_level(1)
     queue = deque([root])
-    while(len(queue) > 0):
+    while len(queue) > 0:
         node = queue.popleft()
         if node.level != clevel:
             clevel += 1
             print()
         print(node.value, end=' ')
-        if node.has_right():
-            right = node.right
-            right.set_level(node.level + 1)
-            queue.append(right)
         if node.has_left():
             left = node.left
             left.set_level(node.level + 1)
             queue.append(left)
+        if node.has_right():
+            right = node.right
+            right.set_level(node.level + 1)
+            queue.append(right)
 
-def dfs(node):
-    if node.has_left():
-        dfs(node.left)
+
+@profile_me
+def dft_exec(node):
+    dft(node)
+
+
+def dft(node) -> None:
+    """Depth first traversal of a binary tree"""
     if node.has_right():
-        dfs(node.right)
+        dft(node.right)
+    if node.has_left():
+        dft(node.left)
     print(node.value, end=' ')
 
-def search(node, val):
+
+@delay_me(delay=.1)
+# @profile_me
+def search_exec(node, val):
+    return search(node, val)
+
+
+def search(node, val) -> bool:
+    """Perform a search on a binary tree
+    
+        :rtype: bool
+    """
     if val == node.value:
         return True
     elif val < node.value and node.has_left():
@@ -57,10 +80,37 @@ def search(node, val):
     else:
         return False
 
-print('breadth first search of a binary tree')
-bfs(node5)
-print('\n\ndepth first search of a binary tree')
-dfs(node5)
-print('\n\nsearching binary tree')
+# node5 is root of a complete balanced binary search tree
+# bfs() should return:
+# 5
+# 38
+# 1469
+print('breadth first traversal of a binary tree')
+bft_exec(node5)
+
+# dft() should return:
+# 5
+# 38
+# 1469
+# print('\n\ndepth first traversal of a binary tree')
+# dft(node5)
+# print('\n\nsearching binary tree')
+# for i in range(10):
+#     print(i, search(node5, i))
+
+num_values = 100_000
+
+tree = Tree(num_values, False)
+tree = Tree(num_values, True)
+
+bft_exec(tree.root)
+dft_exec(tree.root)
 for i in range(10):
-    print(i, search(node5, i))
+    if not search_exec(tree.root, i):
+        print(f'{i} not found')
+    else:
+        print(f'{i} found')
+
+gc.collect()
+
+
--- objects.py
+++ objects.py
@@ -1,8 +1,14 @@
+from random import random
+from math import floor
+from decorators import profile_me
 
 
-class Node():
-    '''Object representing a binary node for a tree data structure'''
-    def __init__(self, value):
+class Node:
+    def __init__(self, value) -> object:
+        """Object representing a binary node for a tree data structure
+        
+            :rtype: object
+        """
         self.value = value
         self.left = None
         self.right = None
@@ -25,3 +31,48 @@ class Node():
 
     def has_right(self):
         return self.right is not None
+
+
+class Tree:
+    def __init__(self, num_values, use_generator: bool) -> object:
+        """Object representing a binary tree"""
+        self.num_values = num_values
+        self.root = None
+        if use_generator:
+            self.generate_tree()
+        else:
+            self.build_tree()
+
+    @profile_me
+    def generate_tree(self):
+        for value in self.values():
+            if self.root is None:
+                self.root = Node(value)
+            else:
+                self.insert(self.root, value)
+
+    @profile_me
+    def build_tree(self):
+        for value in [floor(random()*1000) for value in range(self.num_values)]:
+            if self.root is None:
+                self.root = Node(value)
+            else:
+                self.insert(self.root, value)
+
+    def insert(self, root: Node, value: int):
+        if value < root.value:
+            if root.has_left():
+                self.insert(root.left, value)
+            else:
+                root.left = Node(value)
+        elif value > root.value:
+            if root.has_right():
+                self.insert(root.right, value)
+            else:
+                root.right = Node(value)
+
+    def values(self):
+        for value in range(self.num_values):
+            yield floor(random()*1000)
+
+
